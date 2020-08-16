75eb9534862f8521b59e607d3e70443e14a82186
Carlos Catalan <crlsctalan23@gmail.com>
2020-08-16 22:31:25 +0800

    add react-bootstrap, and favicon and proper title

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.css';
+import { Navbar } from 'react-bootstrap'
+
+function Home() {
+  return (
+    <div className="Home">
+      <Navbar>
+        <Navbar.Brand href="#home">ECStreet</Navbar.Brand>
+        
+      </Navbar>
+
+    </div>
+  );
+}
+
+export default Home;
